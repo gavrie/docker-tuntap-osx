039457d4b886510ffe91d35953a577aec6a41a08
Julian Wundrak <j.wundrak@dotsource.de>
2019-07-16 11:31:45 +0200

    Add docker networks to OSX routing table

--- sbin/docker_tap_up.sh
+++ sbin/docker_tap_up.sh
@@ -15,3 +15,11 @@ hostNetmask='255.255.255.252'
 # Startup local and host tuntap interfaces
 sudo ifconfig $localTapInterface $localGateway up
 docker run --rm --privileged --net=host --pid=host alpine ifconfig $hostTapInterface $hostGateway netmask $hostNetmask up
+
+# Route all docker networks through tap interface
+networks=$(docker network  ls -f 'driver=bridge' --format "{{.Name}}")
+for i in $(echo $networks)
+do
+	subnet=$(docker network inspect $i --format "{{(index .IPAM.Config 0).Subnet}}")
+	sudo route -q add "$subnet" $hostGateway
+done
